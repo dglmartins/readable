cb27a3eb242c422e994cd7ea99ee5af782f64aca
danilomartins <dglmartins@outlook.com>
2017-08-22 10:57:09 -0400

    Post view layout and css

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <div className="header">
+        <section className="header">
           <MdLibraryBooks size={20} className="logo" alt="logo"/>
           <h2 className="title">Readable</h2>
           <ul className="nav">
@@ -16,8 +16,27 @@ class App extends Component {
               <li className="category-nav"><a>udacity</a></li>
               <li className="add-post-nav"><a>new post</a></li>
           </ul>
-        </div>
-
+        </section>
+        <section className="data-view-table-container">
+          <table className="data-view-table">
+            <tbody>
+              <tr className="data-row">
+                <td className="vote-count">Votes: <span>5</span></td>
+                <td className="post-title">This is a post title</td>
+              </tr>
+              <tr className="data-row">
+                <td colSpan="2">
+                  <span className="author">by me</span>
+                  <span className="time-stamp">Created on Saturday, May 1, 12:00pm</span>
+                  <span className="action">edit</span>
+                  <span className="action">delete</span>
+                  <span className="action">vote up</span>
+                  <span className="action">vote down</span>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </section>
       </div>
     );
   }
